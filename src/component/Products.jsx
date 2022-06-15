e28c95fc04c3106f07552f70123e3f2271dfddc1
arsalan khan <akhan492@gmail.com>
2022-06-15 23:03:58 +0530

    "json image data changed"

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Products({ product,onAdd }) {
   return (
     <div className='products'>
-    <img className='small'  src={`../assets/${product.image}`} />
+    <img className='small'  src={product.image} />
     <h1>{product.name}</h1>
     <p><strong>Price</strong> ${product.price}</p>
     <button onClick={()=>onAdd(product)}>Add to Cart</button>
